55283d0a001c2467639520d9cc100386039de713
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2022-06-03 18:35:25 +0200

    add the ability to add new posts to blog, use the useHistory hook to return to the home page after adding a post

--- src/components/Create.js
+++ src/components/Create.js
@@ -1,23 +1,47 @@
 import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
 
 export default function Create() {
 	const [ title, setTitle ] = useState('');
-  const [body, setBody] = useState('');
-  const [author, setAuthor] = useState('Patrycja');
+	const [ body, setBody ] = useState('');
+	const [ author, setAuthor ] = useState('Patrycja');
+	const [ isLoading, setIsLoading ] = useState(false);
+	const history = useHistory();
 
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		const blog = { title, author, body };
+		console.log(blog);
+
+		setIsLoading(true);
+
+		fetch('http://localhost:8000/blogs', {
+			method: 'POST',
+			headers: { 'Content-Type': 'application/json' },
+			body: JSON.stringify(blog)
+		}).then(() => {
+			console.log('new blog added');
+			setIsLoading(false);
+			history.push('/');
+		});
+	};
 	return (
 		<div className="create">
 			<h2>Add a New Blog</h2>
-			<form />
-			<label>Blog title:</label>
-			<input type="text" required value={title} onChange={(e) => setTitle(e.target.value)} />
-			<label>Blog body</label>
-			<textarea required value={body} onChange ={(e) => setBody(e.target.value)}/>
-			<label>Blog author:</label>
-			<select>
-				<option value={author} onChange={(e)=> setAuthor(e.target.value)}>Patrycja</option>
-			</select>
-			<button>Add blog</button>
+			<form onSubmit={handleSubmit}>
+				<label>Blog title:</label>
+				<input type="text" required value={title} onChange={(e) => setTitle(e.target.value)} />
+				<label>Blog body</label>
+				<textarea required value={body} onChange={(e) => setBody(e.target.value)} />
+				<label>Blog author:</label>
+				<select>
+					<option value={author} onChange={(e) => setAuthor(e.target.value)}>
+						Patrycja
+					</option>
+				</select>
+				{!isLoading && <button>Add blog</button>}
+				{isLoading && <button disabled>Adding blog...</button>}
+			</form>
 		</div>
 	);
 }
