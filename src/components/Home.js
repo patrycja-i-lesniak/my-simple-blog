7f955cb9e32771b2d12e1dd676e84d3fbac47e17
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2022-06-03 10:44:27 +0200

    add error handling

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,8 +3,8 @@ import BlogList from './BlogList';
 
 export default function Home() {
 	const [ blogs, setBlogs ] = useState(null);
-
-	const [isLoading, setIsLoading] = useState(true)
+	const [ isLoading, setIsLoading ] = useState(true);
+	const [ error, setError ] = useState(null);
 
 	// const handleDelete = (id) => {
 	// 	const newBlogs = blogs.filter((blog) => blog.id !== id);
@@ -13,22 +13,30 @@ export default function Home() {
 
 	useEffect(() => {
 		// setTimeout only simulates a real fetch api
-		setTimeout(()=>{
-			fetch('http://localhost:8000/blogs')
-			.then((res) => {
-				return res.json();
-			})
-			.then((data) => {
-				setBlogs(data);
-				setIsLoading(false)
-			});
-		}, 1000)
-		
+		setTimeout(() => {
+			fetch('http://localhost:8000/blogs ')
+				.then((res) => {
+					if (!res.ok) {
+						throw Error('could not fetch the data for that resource');
+					}
+					return res.json();
+				})
+				.then((data) => {
+					setBlogs(data);
+					setIsLoading(false);
+					setError(null);
+				})
+				.catch((err) => {
+					setIsLoading(false);
+					setError(err.message);
+				});
+		}, 1000);
 	}, []);
 	return (
 		<div className="home">
+			{error &&<div>{error}</div>}
 			{isLoading && <div>Loading...</div>}
-			{blogs && <BlogList blogs={blogs} title="All blogs!"  />}
+			{blogs && <BlogList blogs={blogs} title="All blogs!" />}
 			{/* <BlogList blogs={blogs.filter(({ author }) => author === 'someone')} title="Someone's blogs!" handleDelete={handleDelete}/> */}
 		</div>
 	);
