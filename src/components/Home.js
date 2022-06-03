065f70f860a37bea26efb824a2174eb830055bc4
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2022-06-03 11:02:47 +0200

    change the function that retrieves data from json-server into a reusable component using custom hooks and import data from the database

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,40 +1,18 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import BlogList from './BlogList';
+import useFetch from 'helpers/useFetch';
 
 export default function Home() {
-	const [ blogs, setBlogs ] = useState(null);
-	const [ isLoading, setIsLoading ] = useState(true);
-	const [ error, setError ] = useState(null);
+	const { data: blogs, isLoading, error } = useFetch('http://localhost:8000/blogs');
 
 	// const handleDelete = (id) => {
 	// 	const newBlogs = blogs.filter((blog) => blog.id !== id);
 	// 	setBlogs(newBlogs);
 	// };
 
-	useEffect(() => {
-		// setTimeout only simulates a real fetch api
-		setTimeout(() => {
-			fetch('http://localhost:8000/blogs ')
-				.then((res) => {
-					if (!res.ok) {
-						throw Error('could not fetch the data for that resource');
-					}
-					return res.json();
-				})
-				.then((data) => {
-					setBlogs(data);
-					setIsLoading(false);
-					setError(null);
-				})
-				.catch((err) => {
-					setIsLoading(false);
-					setError(err.message);
-				});
-		}, 1000);
-	}, []);
 	return (
 		<div className="home">
-			{error &&<div>{error}</div>}
+			{error && <div>{error}</div>}
 			{isLoading && <div>Loading...</div>}
 			{blogs && <BlogList blogs={blogs} title="All blogs!" />}
 			{/* <BlogList blogs={blogs.filter(({ author }) => author === 'someone')} title="Someone's blogs!" handleDelete={handleDelete}/> */}
