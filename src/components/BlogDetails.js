91ac4606ed3a4da0a468d1719fd5cbe9b934ec95
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2022-06-03 18:42:25 +0200

    add the ability to delete posts, delete unused part of code

--- src/components/BlogDetails.js
+++ src/components/BlogDetails.js
@@ -1,11 +1,20 @@
 import useFetch from 'helpers/useFetch';
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import { useParams } from 'react-router-dom';
 
 export default function BlogDetails() {
 	const { id } = useParams();
 	const { data: blog, error, isLoading } = useFetch('http://localhost:8000/blogs/' + id);
+const history= useHistory();
 
+	const handleClick = ()=> {
+fetch('http://localhost:8000/blogs/' + blog.id, {
+	method: 'DELETE'
+}).then(()=>{
+history.push('/');
+})
+	}
 	return (
 		<div className="blog-details">
 			{isLoading && <div>Loading...</div>}
@@ -15,6 +24,7 @@ export default function BlogDetails() {
 					<h2>{blog.title}</h2>
 					<p>Written by {blog.author}</p>
 					<p> {blog.body}</p>
+					<button onClick={handleClick}>delete</button>
 				</article>
 			)}
 		</div>
--- src/components/Home.js
+++ src/components/Home.js
@@ -5,17 +5,11 @@ import useFetch from 'helpers/useFetch';
 export default function Home() {
 	const { data: blogs, isLoading, error } = useFetch('http://localhost:8000/blogs');
 
-	// const handleDelete = (id) => {
-	// 	const newBlogs = blogs.filter((blog) => blog.id !== id);
-	// 	setBlogs(newBlogs);
-	// };
-
 	return (
 		<div className="home">
 			{error && <div>{error}</div>}
 			{isLoading && <div>Loading...</div>}
 			{blogs && <BlogList blogs={blogs} title="All blogs!" />}
-			{/* <BlogList blogs={blogs.filter(({ author }) => author === 'someone')} title="Someone's blogs!" handleDelete={handleDelete}/> */}
 		</div>
 	);
 }
