70ac1ac366f86ed84e46f6458db73ba23973bbbe
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2022-06-03 13:45:04 +0200

    add link to BlogList

--- src/components/BlogList.js
+++ src/components/BlogList.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function BlogList({ blogs, title }) {
 	return (
@@ -6,9 +7,12 @@ export default function BlogList({ blogs, title }) {
 			<h2 className="blog-title">{title}</h2>
 			{blogs.map((blog) => (
 				<div className="blog-preview" key={blog.id}>
-					<h2>{blog.title}</h2>
-					<p>{blog.body}</p>
-					<p>Written by {blog.author}</p>
+					<Link to={`/blogs/${blog.id}`}>
+						<h2>{blog.title}</h2>
+						<p>{blog.body}</p>
+						<p>Written by {blog.author}</p>
+					</Link>
+
 					{/* <button onClick={()=> handleDelete(blog.id)}>delete blog</button> */}
 				</div>
 			))}
