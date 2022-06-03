2173c898b30c1fb28d1b1116bfc4e218084ec899
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2022-06-03 16:09:42 +0200

    create a simple new post form and style it

--- src/components/Create.js
+++ src/components/Create.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 export default function Create() {
-  return (
-    <div className='create'>
-      <h2>Add a New Blog</h2>
-    </div>
-  )
+	const [ title, setTitle ] = useState('');
+  const [body, setBody] = useState('');
+  const [author, setAuthor] = useState('Patrycja');
+
+	return (
+		<div className="create">
+			<h2>Add a New Blog</h2>
+			<form />
+			<label>Blog title:</label>
+			<input type="text" required value={title} onChange={(e) => setTitle(e.target.value)} />
+			<label>Blog body</label>
+			<textarea required value={body} onChange ={(e) => setBody(e.target.value)}/>
+			<label>Blog author:</label>
+			<select>
+				<option value={author} onChange={(e)=> setAuthor(e.target.value)}>Patrycja</option>
+			</select>
+			<button>Add blog</button>
+		</div>
+	);
 }
