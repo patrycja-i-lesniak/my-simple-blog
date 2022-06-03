171bb24e7a0be8a79909320e894a0ad31fd530f9
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2022-06-03 11:35:24 +0200

    install react router and add them to aplication

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
-import Navbar from 'components/Navbar'
-import Home from 'components/Home'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+import Navbar from 'components/Navbar';
+import Home from 'components/Home';
+import Create from 'components/Create';
 
 function App() {
-  return (
-    <div className="App">
-      <Navbar/>
-      <div className='content'>
-        <Home/>
-      </div>
-    </div>
-  );
+	return (
+		<Router>
+			<div className="App">
+				<Navbar />
+				<div className="content">
+					<Switch>
+						<Route exact path="/">
+							<Home />
+						</Route>
+						<Route path="/create">
+							<Create />
+						</Route>
+						{/* <Route path="/contact"><Contact/></Route>
+						<Route path="/about"><About/></Route> */}
+					</Switch>
+				</div>
+			</div>
+		</Router>
+	);
 }
 
 export default App;
