c04ce1363eda43e7da59213b5de4d6bce6980d43
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2022-06-14 08:17:08 +0200

    add Login and Signup page

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,6 +8,8 @@ export default function Navbar () {
       <div className='links'>
         <Link to="/">Home</Link>
         <Link to="/create">New Blog</Link>
+        <Link to="/login">Login</Link>
+        <Link to="/signup">Sign up</Link>
         </div>
     </nav>
   )
