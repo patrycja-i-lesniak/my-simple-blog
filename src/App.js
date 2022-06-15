4fa88be371113bbd826837805def7f8b9a7c1601
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2022-06-15 18:47:48 +0200

    create a Dashboard with Notifications and Project List

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 
 // components
-import Navbar from 'components/Navbar';
+import Navbar from './components/layout/Navbar';
+import Dashboard from '../src/components/dashboard/Dashboard';
 
 // pages
-import Home from 'pages/Home';
-import Create from 'pages/Create';
-import BlogDetails from 'pages/BlogDetails';
-import Login from 'pages/Login';
-import Signup from 'pages/Signup';
+// import Home from './pages/Home';
+import Create from './pages/Create';
+import BlogDetails from './pages/BlogDetails';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
 
 function App() {
 	return (
@@ -17,10 +18,10 @@ function App() {
 			<Navbar />
 			<div className="content">
 				<Routes>
-					<Route exact path="/" element={<Home />} />
+					<Route path="/" element={<Dashboard />} />
 					<Route path="/login" element={<Login />} />
 					<Route path="/signup" element={<Signup />} />
-					<Route path="/blogs/:id" element={<BlogDetails />} />
+					<Route path="/blogs/:id/*" element={<BlogDetails />} />
 					<Route path="create" element={<Create />} />
 				</Routes>
 			</div>
---
+++ src/components/projects/ProjectList.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+export default function ProjectList() {
+	return (
+		<div className="project-list section">
+			<div className="card z-depth-0 project-summary">
+				<div className="card-content grey-text text-darken-3">
+					<span className="card-title">BlogTitle</span>
+					<p> Posted by Patrycja</p>
+					<p p className="grey-text">
+						3-rd September
+					</p>
+				</div>
+			</div>
+			<div className="card z-depth-0 project-summary">
+				<div className="card-content grey-text text-darken-3">
+					<span className="card-title">BlogTitle</span>
+					<p> Posted by Patrycja</p>
+					<p p className="grey-text">
+						3-rd September
+					</p>
+				</div>
+			</div>
+			<div className="card z-depth-0 project-summary">
+				<div className="card-content grey-text text-darken-3">
+					<span className="card-title">BlogTitle</span>
+					<p> Posted by Patrycja</p>
+					<p p className="grey-text">
+						3-rd September
+					</p>
+				</div>
+			</div>
+		</div>
+	);
+}
