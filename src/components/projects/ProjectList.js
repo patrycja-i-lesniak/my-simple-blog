31a93aa20ce1262109a4fd80bae6ba2f28f51823
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2022-06-15 18:54:07 +0200

    create ProjectSummary and simplify Project  List

--- src/components/projects/ProjectList.js
+++ src/components/projects/ProjectList.js
@@ -1,35 +1,13 @@
 import React from 'react';
+import ProjectSummary from './ProjectSummary';
 
 export default function ProjectList() {
 	return (
 		<div className="project-list section">
-			<div className="card z-depth-0 project-summary">
-				<div className="card-content grey-text text-darken-3">
-					<span className="card-title">BlogTitle</span>
-					<p> Posted by Patrycja</p>
-					<p p className="grey-text">
-						3-rd September
-					</p>
-				</div>
-			</div>
-			<div className="card z-depth-0 project-summary">
-				<div className="card-content grey-text text-darken-3">
-					<span className="card-title">BlogTitle</span>
-					<p> Posted by Patrycja</p>
-					<p p className="grey-text">
-						3-rd September
-					</p>
-				</div>
-			</div>
-			<div className="card z-depth-0 project-summary">
-				<div className="card-content grey-text text-darken-3">
-					<span className="card-title">BlogTitle</span>
-					<p> Posted by Patrycja</p>
-					<p p className="grey-text">
-						3-rd September
-					</p>
-				</div>
-			</div>
+			<ProjectSummary/>
+			<ProjectSummary/>
+			<ProjectSummary/>
+			<ProjectSummary/>
 		</div>
 	);
 }
