2de3a8b8a986fa420eca1720e93a844ec452d89d
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2022-06-17 19:02:31 +0200

    add store to app

--- src/index.js
+++ src/index.js
@@ -1,15 +1,22 @@
 import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import {createStore} from 'redux';
 import './index.css';
 import App from './App';
+import rootReducer from './store/reducers/rootReducer';
+import {Provider} from 'react-redux';
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
+const store = createStore(rootReducer);
 root.render(
 	<StrictMode>
 		<BrowserRouter>
-			<App />
+		<Provider store={store}>
+				<App />
+		</Provider>
+		
 		</BrowserRouter>
 	</StrictMode>
 );
