1c4a80ba712a61b4c6e5630a3ced799d73c403b4
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2022-06-20 20:16:29 +0200

    change functional component to class component

--- src/components/blogs/CreateBlog.js
+++ src/components/blogs/CreateBlog.js
@@ -1,45 +1,60 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { createBlog } from '../../store/actions/blogActions';
 
-export default function CreateBlog() {
-	const [ formData, setFormData ] = useState({
+class CreateBlog extends Component {
+	state = {
 		title: '',
 		body: '',
 		author: ' '
-	});
+	};
 
-	function handleChange(e) {
-		setFormData({ ...formData, [e.target.id]: e.target.value });
-	}
-	function handleSubmit(e) {
+	handleChange = (e) => {
+		this.setState({
+			[e.target.id]: e.target.value
+		});
+	};
+
+	handleSubmit = (e) => {
 		e.preventDefault();
-		console.log(formData)
-   
+		this.props.createBlog(this.state);
+	};
+	render() {
+		return (
+			<div className="container">
+				<form onSubmit={this.handleSubmit} className="white">
+					<h5 className="grey-text text-darken-3">Create New Blog</h5>
+					<div className="input-field">
+						<label htmlFor="title">Title</label>
+
+						<input type="text" id="title" onChange={this.handleChange} />
+					</div>
+					<div className="input-field">
+						<label htmlFor="body">Blog text</label>
+						<textarea
+							type="text"
+							id="body"
+							className="materialize-textarea"
+							onChange={this.handleChange}
+						/>
+					</div>
+					<div className="input-field">
+						<label htmlFor="author">Author</label>
+
+						<input type="text" id="title" onChange={this.handleChange} />
+					</div>
+					<div className="input-field">
+						<button className="btn red darken-4 z-depth-0">Create</button>
+					</div>
+				</form>
+			</div>
+		);
 	}
-	return (
-		<div className="container">
-			<form onSubmit={(e) => handleSubmit(e)} className="white">
-				<h5 className="grey-text text-darken-3">Sign In</h5>
-				<div className="input-field">
-					<label htmlFor="title">Title</label>
-					<input type="text" id="title" onChange={(e) => handleChange(e)} />
-				</div>
-				<div className="input-field">
-					<label htmlFor="body">Blog text</label>
-					<textarea
-						type="text"
-						id="body"
-						className="materialize-textarea"
-						onChange={(e) => handleChange(e)}
-					/>
-				</div>
-				<div className="input-field">
-					<label htmlFor="author">Author</label>
-					<input type="text" id="author" onChange={(e) => handleChange(e)} />
-				</div>
-				<div className="input-field">
-					<button className="btn red darken-4 z-depth-0">Create</button>
-				</div>
-			</form>
-		</div>
-	);
 }
+
+const mapDispatchToProps = (dispatch) => {
+	return {
+		createBlog: (blog) => dispatch(createBlog(blog))
+	};
+};
+export default connect(null, mapDispatchToProps)(CreateBlog);
