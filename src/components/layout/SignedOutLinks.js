fc898cb4173afcd424052ecd7401704e6f75122f
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2022-06-24 10:38:05 +0200

    delete unnecessary imports

--- src/components/layout/SignedOutLinks.js
+++ src/components/layout/SignedOutLinks.js
@@ -1,12 +1,12 @@
 import React, { useState, useRef } from 'react';
 import { NavLink } from 'react-router-dom';
-import { signup, useAuth, login } from '../../config/firebaseConfig';
+import { useAuth } from '../../config/firebaseConfig';
 
 export default function SignedOutLinks() {
 	const [ loading, setLoading ] = useState(false);
 	const currentUser = useAuth();
 	return (
-		<ul className="right hide-on-med-and-down" hidden={loading || currentUser} >
+		<ul className="right hide-on-med-and-down" hidden={loading || currentUser}>
 			<li>
 				<NavLink to="/signup">Signup</NavLink>
 			</li>
