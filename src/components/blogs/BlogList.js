fb26e16687e59301cc48b7c2cc827a153b72fcfe
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2022-06-27 20:43:20 +0200

    get data from firebase

--- src/components/blogs/BlogList.js
+++ src/components/blogs/BlogList.js
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { getFirestore, onSnapshot, collection } from 'firebase/firestore';
 import BlogSummary from './BlogSummary';
 
 export default function BlogList() {
+	const [ blogs, setBlogs ] = useState([]);
+	const db = getFirestore();
+	console.log(blogs);
+
+	useEffect(() => {
+		onSnapshot(collection(db, 'blogs'), (snapshot) => {
+			setBlogs(snapshot.docs.map((doc) =>({ ...doc.data(), id: doc.id })));
+		});
+	}, []);
+	
 	return (
 		<div className="blog-list section">
-			<BlogSummary/>
-			<BlogSummary/>
-			<BlogSummary/>
-			<BlogSummary/>
+			{blogs &&
+				blogs.map((blog) => {
+					return <BlogSummary blog={blog} key={blog.id} />;
+				})}
 		</div>
 	);
 }
