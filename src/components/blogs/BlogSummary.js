edb551834fd0e5ad48639cfc49bea8221b7f77fc
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2022-06-28 21:43:16 +0200

    add navigate to BlogDetails and conditional slice

--- src/components/blogs/BlogSummary.js
+++ src/components/blogs/BlogSummary.js
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
-export default function BlogSummary() {
+export default function BlogSummary({ blog }) {
+	const navigate = useNavigate();
+	const handleNavigate = () => {
+		navigate('/blog/:id');
+	};
 	return (
 		<div className="card z-depth-0 blog-summary">
 			<div className="card-content grey-text text-darken-3">
-				<span className="card-title">Blog Title</span>
-				<p> Written by Patrycja</p>
-				<p className="grey-text">3-rd September, lorem ipsum, lorem ipsum</p>
+				<span className="card-title red-text text-darken-4">{blog.title}</span>
+				<p>{blog.author}</p>
+				<p>{blog.date}</p>
+				{blog.body.length > 200 && (
+					<p className="grey-text">{blog.body.slice(0, 200)}...</p>
+				)}
+				<button onClick={handleNavigate} className="btn red darken-4 z-depth-0">
+					Read more
+				</button>
 			</div>
 		</div>
 	);
