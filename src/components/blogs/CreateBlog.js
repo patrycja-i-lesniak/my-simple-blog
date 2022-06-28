7d79a3040fda2b6c60834c03b1108c72a1fc0a54
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2022-06-28 20:47:50 +0200

    rebuild CreateBlog component

--- src/components/blogs/CreateBlog.js
+++ src/components/blogs/CreateBlog.js
@@ -1,60 +1,56 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { createBlog } from '../../store/actions/blogActions';
+import React, { useState } from 'react';
+import { addDoc, collection, doc } from 'firebase/firestore';
+import { db, colRef } from '../../config/firebaseConfig';
 
-class CreateBlog extends Component {
-	state = {
+export default function CreateBlog() {
+	const [ formData, setFormData ] = useState({
 		title: '',
 		body: '',
-		author: ' '
-	};
+		author: ' ',
+		date: ''
+	});
 
-	handleChange = (e) => {
-		this.setState({
-			[e.target.id]: e.target.value
-		});
-	};
-
-	handleSubmit = (e) => {
-		e.preventDefault();
-		this.props.createBlog(this.state);
-	};
-	render() {
-		return (
-			<div className="container">
-				<form onSubmit={this.handleSubmit} className="white">
-					<h5 className="grey-text text-darken-3">Create New Blog</h5>
-					<div className="input-field">
-						<label htmlFor="title">Title</label>
-
-						<input type="text" id="title" onChange={this.handleChange} />
-					</div>
-					<div className="input-field">
-						<label htmlFor="body">Blog text</label>
-						<textarea
-							type="text"
-							id="body"
-							className="materialize-textarea"
-							onChange={this.handleChange}
-						/>
-					</div>
-					<div className="input-field">
-						<label htmlFor="author">Author</label>
-
-						<input type="text" id="title" onChange={this.handleChange} />
-					</div>
-					<div className="input-field">
-						<button className="btn red darken-4 z-depth-0">Create</button>
-					</div>
-				</form>
-			</div>
-		);
+	function handleChange(e) {
+		setFormData({ ...formData, [e.target.id]: e.target.value });
 	}
-}
 
-const mapDispatchToProps = (dispatch) => {
-	return {
-		createBlog: (blog) => dispatch(createBlog(blog))
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		const docRef = await addDoc(colRef, formData);
+		setFormData(e.target.reset());
+		console.log(`New article with ID ${docRef.id} added`);
 	};
-};
-export default connect(null, mapDispatchToProps)(CreateBlog);
+
+	return (
+		<div className="container ">
+			<form onSubmit={(e) => handleSubmit(e)} className="white add">
+				<h5 className="grey-text text-darken-3">Create New Blog</h5>
+				<div className="input-field">
+					<label htmlFor="title">Title</label>
+					<input type="text" id="title" onChange={(e) => handleChange(e)} />
+				</div>
+				<div className="input-field">
+					<label htmlFor="body">Blog text</label>
+					<textarea
+						type="text"
+						id="body"
+						className="materialize-textarea"
+						onChange={(e) => handleChange(e)}
+					/>
+				</div>
+				<div className="input-field">
+					<label htmlFor="author">Author</label>
+					<input type="text" id="author" onChange={(e) => handleChange(e)} />
+				</div>
+			<div className="input-field">
+					<label  htmlFor="date"> </label>
+					<input type="date" id="date" onChange={(e) => handleChange(e)} />
+				</div>
+				
+				<div className="input-field">
+					<button className="btn red darken-4 z-depth-0">Create</button>
+				</div>
+			</form>
+		</div>
+	);
+}
