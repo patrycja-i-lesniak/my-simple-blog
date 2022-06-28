4db227d5bdcdfb4caace0af46347e664a9790795
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2022-06-28 20:48:00 +0200

    add footer

---
+++ src/components/layout/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <footer className="page-footer grey darken-3">
+    
+          <div className="footer-copyright">
+            <div className="container">
+            © 2022 Coded by Patrycja Leśniak
+            <a className="grey-text text-lighten-4 right" href="https://github.com/patrycja-i-lesniak">GitHub</a>
+            </div>
+          </div>
+        </footer>
+  )
+}
