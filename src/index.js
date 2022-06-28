f65446d237abeded1408ccbdbfc0601d33a3efb2
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2022-06-28 20:46:44 +0200

    delete unued imports and part of code

--- src/index.js
+++ src/index.js
@@ -1,22 +1,17 @@
 import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import {createStore} from 'redux';
 import './index.css';
 import App from './App';
-import rootReducer from './store/reducers/rootReducer';
-import {Provider} from 'react-redux';
 
 const rootElement = document.getElementById('root');
+
 const root = createRoot(rootElement);
-const store = createStore(rootReducer);
+
 root.render(
 	<StrictMode>
 		<BrowserRouter>
-		<Provider store={store}>
-				<App />
-		</Provider>
-		
+			<App />
 		</BrowserRouter>
 	</StrictMode>
 );
