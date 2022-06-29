c8cef9246bbd0e3e98d78da47c63dd286b5fa97c
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2022-06-29 20:07:44 +0200

    navigate to homepage after sign up

--- src/components/auth/SignUp.js
+++ src/components/auth/SignUp.js
@@ -1,5 +1,6 @@
 import React, { useState, useRef } from 'react';
-import { signup, useAuth, logout, login } from '../../config/firebaseConfig';
+import { signup, useAuth } from '../../config/firebaseConfig';
+import { useNavigate } from 'react-router-dom';
 
 export default function SignUp() {
 	const [ loading, setLoading ] = useState(false);
@@ -7,42 +8,37 @@ export default function SignUp() {
 	const emailRef = useRef();
 	const passwordRef = useRef();
 	const firstNameRef = useRef();
-	const lastNameRef= useRef();
+	const lastNameRef = useRef();
+	const navigate = useNavigate();
 
 	async function handleSignup() {
 		setLoading(true);
 		try {
-		await signup(emailRef.current.value, passwordRef.current.value, firstNameRef.current.value, lastNameRef.current.value);
-		console.log('Register as:', firstNameRef.current.value, lastNameRef.current.value )
-		} catch {
-		alert("Error!");
+			await signup(
+				emailRef.current.value,
+				passwordRef.current.value,
+				firstNameRef.current.value,
+				lastNameRef.current.value
+			);
+			console.log(
+				'Register and logged as:',
+				firstNameRef.current.value,
+				lastNameRef.current.value
+			);
+		} catch (error) {
+			alert('Error!');
 		}
 		setLoading(false);
+		navigate('/');
 	}
-
-		async function handleLogin() {
-		setLoading(true);
-		try {
-		await login(emailRef.current.value, passwordRef.current.value);
-		console.log('Currently logged in as:', firstNameRef.current.value, lastNameRef.current.value )
-		} catch {
-		alert("Error!");
-		}
-		setLoading(false);
-	}
-
-	async function handleLogout() { 
-			setLoading(true)
-		try{
-			await logout();
-	} catch {
-		alert('Error!');
-	} 	setLoading(false)
-}
 
 	return (
 		<div className="container">
-			{currentUser ? <p>Hello { firstNameRef.current.value} {lastNameRef.current.value}!</p>: null}
+			{currentUser ? (
+				<p>
+					Hello {firstNameRef.current.value} {lastNameRef.current.value}!
+				</p>
+			) : null}
 			<div id="fields" className="white">
 				<h5 className="grey-text text-darken-3">Sign Up</h5>
 				<div className="input-field">
@@ -51,11 +47,11 @@ export default function SignUp() {
 				</div>
 				<div className="input-field">
 					<label htmlFor="firstName">First Name</label>
-					<input type="text" id="firstName"  ref={firstNameRef}/>
+					<input type="text" id="firstName" ref={firstNameRef} />
 				</div>
 				<div className="input-field">
 					<label htmlFor="lastName">Last Name</label>
-					<input type="text" id="lastName"  ref={lastNameRef} />
+					<input type="text" id="lastName" ref={lastNameRef} />
 				</div>
 				<div className="input-field">
 					<label htmlFor="password">Password</label>
@@ -69,16 +65,6 @@ export default function SignUp() {
 					>
 						Sign Up
 					</button>
-					
-
-<button
-						disabled={loading || currentUser}
-						className="btn red darken-4 z-depth-0"
-						onClick={handleLogin}
-					>
-						Log In
-					</button>
-					<button disabled={loading || !currentUser} onClick={handleLogout}>Log Out</button>
 				</div>
 			</div>
 		</div>
