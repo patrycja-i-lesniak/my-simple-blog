aa4d9f95db395bfe22a807992fad59befa4760eb
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2022-06-29 20:15:49 +0200

    add required and min length to input field

--- src/components/blogs/CreateBlog.js
+++ src/components/blogs/CreateBlog.js
@@ -1,6 +1,7 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import { addDoc, collection, doc } from 'firebase/firestore';
 import { db, colRef } from '../../config/firebaseConfig';
+import { useAuth, login } from '../../config/firebaseConfig';
 
 export default function CreateBlog() {
 	const [ formData, setFormData ] = useState({
@@ -9,6 +10,8 @@ export default function CreateBlog() {
 		author: ' ',
 		date: ''
 	});
+	// const currentUser = useAuth();
+	// console.log(currentUser.email)
 
 	function handleChange(e) {
 		setFormData({ ...formData, [e.target.id]: e.target.value });
@@ -27,7 +30,13 @@ export default function CreateBlog() {
 				<h5 className="grey-text text-darken-3">Create New Blog</h5>
 				<div className="input-field">
 					<label htmlFor="title">Title</label>
-					<input type="text" id="title" onChange={(e) => handleChange(e)} />
+					<input
+						type="text"
+						id="title"
+						onChange={(e) => handleChange(e)}
+						minlength="5"
+						required
+					/>
 				</div>
 				<div className="input-field">
 					<label htmlFor="body">Blog text</label>
@@ -36,17 +45,25 @@ export default function CreateBlog() {
 						id="body"
 						className="materialize-textarea"
 						onChange={(e) => handleChange(e)}
+						minlength="100"
+						required
 					/>
 				</div>
 				<div className="input-field">
 					<label htmlFor="author">Author</label>
-					<input type="text" id="author" onChange={(e) => handleChange(e)} />
+					<input
+						type="text"
+						id="author"
+						onChange={(e) => handleChange(e)}
+						minlength="5"
+						required
+					/>
 				</div>
-			<div className="input-field">
-					<label  htmlFor="date"> </label>
-					<input type="date" id="date" onChange={(e) => handleChange(e)} />
+				<div className="input-field">
+					<label htmlFor="date"> </label>
+					<input type="date" id="date" onChange={(e) => handleChange(e)} required />
 				</div>
-				
+
 				<div className="input-field">
 					<button className="btn red darken-4 z-depth-0">Create</button>
 				</div>
