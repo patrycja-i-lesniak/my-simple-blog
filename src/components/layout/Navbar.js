5ec6bddf25d2c584fd8c6b72d05288df5e8c3f5a
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2022-07-06 10:40:27 +0200

    rebuild navbar

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,38 +1,20 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import SignedInLiks from '../layout/SignedInLinks';
 import SignedOutLinks from '../layout/SignedOutLinks';
 
 export default function Navbar() {
 	return (
-		<nav className="nav-wrapper grey darken-3">
-			<div className="container">
-				<Link to="/" className="brand-logo">
-					AwesomeBlog
-				</Link>
-        <SignedInLiks/>
-        <SignedOutLinks/>
-				{/* <Link to="/" className="brand-logo">
-					Home
-				</Link>
-				<Link to="/create">New Blog</Link>
-				<Link to="/login">Login</Link>
-				<Link to="/signup">Sign up</Link>*/}
+		<nav className="nav-extended">
+			<div className="nav-wrapper orange darken-4 ">
+				<div className="container">
+					<NavLink to="/" className="brand-logo ">
+						My Site
+					</NavLink>
+					<SignedInLiks />
+					<SignedOutLinks />
+				</div>
 			</div>
 		</nav>
 	);
 }
-
-// export default function Navbar () {
-//   return (
-//     <nav className='navbar'>
-//       <h1>Awesome Blog</h1>
-//       <div className='links'>
-//         <Link to="/">Home</Link>
-//         <Link to="/create">New Blog</Link>
-//         <Link to="/login">Login</Link>
-//         <Link to="/signup">Sign up</Link>
-//         </div>
-//     </nav>
-//   )
-// }
