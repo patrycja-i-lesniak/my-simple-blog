2c9013d6d41d4f74730f11fa0d46da9e693c2abd
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2022-07-07 20:55:18 +0200

    delete search box from dashboard

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import BlogList from '../blogs/BlogList';
-import SearchBox from '../layout/SearchBox';
 
 export default function Dashboard(props) {
 	const { blogs } = props;
 
 	return (
 		<div className="dashboard container ">
-				<SearchBox />
 				<BlogList blogs={blogs} />
 		</div>
 	);
