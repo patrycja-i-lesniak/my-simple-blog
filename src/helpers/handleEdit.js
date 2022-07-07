c0d7e048292820ace08b76c61dc856e880c6f60a
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2022-07-07 21:31:33 +0200

    add handleEdit to helpers

---
+++ src/helpers/handleEdit.js
@@ -0,0 +1,16 @@
+import { setDoc, doc } from 'firebase/firestore';
+import { db } from '../config/firebaseConfig';
+
+ const handleEdit = async (id) => {
+		const title = prompt('enter title');
+		const body = prompt('enter body');
+		const author = prompt('enter author');
+		const date = prompt('enter date');
+
+		const docRef = doc(db, 'blogs', id);
+		const payload = { title, body, date, author };
+
+		setDoc(docRef, payload);
+	};
+
+    export default handleEdit;
