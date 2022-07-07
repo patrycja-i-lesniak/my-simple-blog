f0da16533bc7b17daffaf67c56e3e4632e92498b
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2022-07-07 20:53:56 +0200

    add bulk file with exports

---
+++ src/components/index.js
@@ -0,0 +1,13 @@
+export {default as Login} from './auth/Login';
+export {default as SignUp} from './auth/SignUp';
+export {default as Profile} from './auth/Profile';
+
+export {default as Navbar} from './layout/Navbar';
+export {default as Footer} from './layout/Footer';
+export {default as SearchBox} from './layout/SearchBox';
+
+export {default as Dashboard} from './dashboard/Dashboard';
+
+export {default as BlogDetails} from './blogs/BlogDetails';
+export {default as CreateBlog} from './blogs/CreateBlog';
+export {default as NewPage} from './blogs/NewPage';
