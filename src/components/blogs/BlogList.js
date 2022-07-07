2ba4043afbe351188407c1af6b8857d5c3821fd0
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2022-07-07 21:35:05 +0200

    delete unnecessary imports and clg, add button

--- src/components/blogs/BlogList.js
+++ src/components/blogs/BlogList.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { onSnapshot, collection } from 'firebase/firestore';
 import BlogSummary from './BlogSummary';
-import { db } from '../../config/firebaseConfig';
+import { db} from '../../config/firebaseConfig';
+import { handleQueryDelete } from 'helpers';
 
 export default function BlogList() {
 	const [ blogs, setBlogs ] = useState([]);
@@ -14,6 +15,12 @@ export default function BlogList() {
 
 	return (
 		<div className="blog-list section">
+			<button
+				className="btn orange z-depth-0  grey-text text-darken-3"
+				onClick={handleQueryDelete}
+			>
+				query delete
+			</button>
 			{blogs &&
 				blogs.map((blog) => {
 					return <BlogSummary blog={blog} key={blog.id} />;
