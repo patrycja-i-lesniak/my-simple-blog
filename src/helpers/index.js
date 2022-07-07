cb63e076d3ebb0918ff8870935dc8ad6e03977d2
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2022-07-07 21:33:24 +0200

    add bulk file with exports

---
+++ src/helpers/index.js
@@ -0,0 +1,4 @@
+export { default as handleEdit } from './handleEdit';
+export { default as handleDelete } from './handleDelete';
+export { default as handleQuerySearch } from './handleQuerySearch';
+export { default as handleQueryDelete } from './handleQueryDelete';
