4ab9487e93642c0e52fb1b6d067f6b1cce2ada12
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2022-07-09 22:45:33 +0200

    rebuild and restyle navigation

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,20 +1,54 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { NavLink } from 'react-router-dom';
 import SignedInLiks from '../layout/SignedInLinks';
 import SignedOutLinks from '../layout/SignedOutLinks';
+import { useAuth, logout, colRef } from '../../config/firebaseConfig';
+import { Icon } from 'react-materialize';
 
 export default function Navbar() {
+	const currentUser = useAuth();
+	const [ photoURL, setPhotoURL ] = useState(
+		'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png'
+	);
+
+	useEffect(
+		() => {
+			{
+				currentUser && currentUser.photoURL
+					? setPhotoURL(currentUser.photoURL)
+					: setPhotoURL(photoURL);
+			}
+		},
+		[ currentUser ]
+	);
 	return (
-		<nav className="nav-extended">
-			<div className="nav-wrapper orange darken-4 ">
-				<div className="container">
-					<NavLink to="/" className="brand-logo ">
-						My Site
-					</NavLink>
-					<SignedInLiks />
-					<SignedOutLinks />
-				</div>
-			</div>
-		</nav>
+		<div>
+			<nav>
+				<Icon right className="grey-text">
+					search
+				</Icon>
+				<NavLink to="/" className="black-text header">
+					SIMPLE VEGE LIFE
+				</NavLink>
+				<SignedInLiks />
+				<SignedOutLinks />
+			</nav>
+			<subnav>
+				<NavLink to="/">
+					Home<Icon right className="grey-text icon">
+						{' '}
+						keyboard_arrow_down
+					</Icon>
+				</NavLink>
+				<NavLink to="/">
+					Recipes<Icon right className="grey-text icon">
+						{' '}
+						keyboard_arrow_down
+					</Icon>
+				</NavLink>
+				<NavLink to="/">About</NavLink>
+				<NavLink to="/">Contact</NavLink>
+			</subnav>
+		</div>
 	);
 }
