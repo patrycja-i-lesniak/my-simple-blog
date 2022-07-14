327f698dfa29ac1543ce359db8bec874dd412e92
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2022-07-14 11:50:22 +0200

    rebuild and restyle footer

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import {BsGithub} from 'react-icons/bs';
 
 export default function Footer() {
   return (
@@ -7,7 +8,7 @@ export default function Footer() {
           <div className="footer-copyright">
             <div className="container">
             © 2022 Coded by Patrycja Leśniak
-            <a className="grey-text text-lighten-4 right" href="https://github.com/patrycja-i-lesniak">GitHub</a>
+            <a className="grey-text text-lighten-4 right" href="https://github.com/patrycja-i-lesniak"><BsGithub className='github'/></a>
             </div>
           </div>
         </footer>
