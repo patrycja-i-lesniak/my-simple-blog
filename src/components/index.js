ed8db371089ead56b6783b105e60de5a3f96f883
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2022-07-15 21:41:07 +0200

    export components

--- src/components/index.js
+++ src/components/index.js
@@ -2,12 +2,14 @@ export {default as Login} from './auth/Login';
 export {default as SignUp} from './auth/SignUp';
 export {default as Profile} from './auth/Profile';
 
-export {default as Navbar} from './layout/Navbar';
+export {default as NavBar} from './layout/Navbar';
 export {default as Footer} from './layout/Footer';
 export {default as SearchBox} from './layout/SearchBox';
-
-export {default as Dashboard} from './dashboard/Dashboard';
+export {default as Submenu} from './layout/Submenu';
+export {default as Slider} from './layout/Slider';
+export {default as Dashboard} from './layout/Dashboard';
 
 export {default as BlogDetails} from './blogs/BlogDetails';
 export {default as CreateBlog} from './blogs/CreateBlog';
+export {default as BlogList} from './blogs/BlogList';
 export {default as NewPage} from './blogs/NewPage';
