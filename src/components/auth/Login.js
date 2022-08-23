b2bd952f5a00f76e7365f17d224ac4b61f52d440
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2022-08-23 13:53:42 +0200

    rebuild login section, add the ability to log in with a google account

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,69 +1,106 @@
-import React, { useState, useEffect } from 'react';
-import { auth, logInWithEmailAndPassword } from 'config/firebaseConfig';
-import { useAuthState } from 'react-firebase-hooks/auth';
-import { useNavigate } from 'react-router-dom';
-import { Form, Button } from 'react-bootstrap';
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { Form, Button } from "react-bootstrap";
+import { signInWithEmailAndPassword, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+
+import { auth } from "../../config/firebase";
 
 export default function Login() {
-	const [ email, setEmail ] = useState('');
-	const [ password, setPassword ] = useState('');
-	const [ user, loading, error ] = useAuthState(auth);
-	const navigate = useNavigate();
+  const [data, setData] = useState({});
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
 
-	useEffect(
-		() => {
-			if (error) {
-				console.log(error.message);
-			}
-			if (loading) {
-				// loader
-				return;
-			}
-			if (user) {
-				console.log('Signed In User', user.email);
-				navigate('/');
-			}
-		},
-		[ user, loading, error, navigate ]
-	);
+  let googleProvider = new GoogleAuthProvider();
 
-	return (
-		<div className="form-container">
-			<h5>Login</h5>
+  const handleInput = (e) => {
+    let newInput = { [e.target.name]: e.target.value };
 
-			<Form>
-				<Form.Group className="mb-3" controlId="email">
-					<Form.Label>Email address</Form.Label>
-					<Form.Control
-						type="email"
-						placeholder="Enter email"
-						value={email}
-						onChange={(e) => setEmail(e.target.value)}
-					/>
-					<Form.Text className="text-muted">
-						We'll never share your email with anyone else.
-					</Form.Text>
-				</Form.Group>
+    setData({ ...data, ...newInput });
+  };
 
-				<Form.Group className="mb-3" controlId="password">
-					<Form.Label>Password</Form.Label>
-					<Form.Control
-						type="password"
-						placeholder="Enter password"
-						value={password}
-						onChange={(e) => setPassword(e.target.value)}
-					/>
-				</Form.Group>
+  // Login
+  const handleLogin = (e) => {
+    e.preventDefault();
+    setLoading(true);
+    signInWithEmailAndPassword(auth, data.email, data.password)
+      .then((response) => {
+        console.log(response.user);
+        navigate("/profile");
+      })
+      .catch((err) => {
+        alert(err.message);
+        console.log(err.code);
+      });
+    setLoading(false);
+  };
 
-				<Form.Group className="d-grid d-md-flex justify-content-md-end">
-					<Button
-						variant="info"
-						onClick={() => logInWithEmailAndPassword(email, password)}
-					>
-						Login
-					</Button>
-				</Form.Group>
-			</Form>
-		</div>
-	);
+  // Login with Google
+  const handleLoginWithGoogle = (e) => {
+    e.preventDefault();
+    setLoading(true);
+    signInWithPopup(auth, googleProvider)
+      .then((response) => {
+        console.log(response.user);
+        navigate("/profile");
+      })
+      .catch((err) => {
+        alert(err.message);
+        console.log(err.code);
+      });
+    setLoading(false);
+  };
+
+  return (
+    <div className="form-container">
+      <h5 className="grey-text text-darken-3">Login</h5>
+
+      <Form>
+        <Form.Group className="mb-3" controlId="email">
+          <Form.Label>
+            Email <span>*</span>
+          </Form.Label>
+          <Form.Control
+            type="email"
+            name="email"
+            placeholder="Enter email"
+            onChange={(e) => handleInput(e)}
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="password">
+          <Form.Label>
+            Password <span>*</span>
+          </Form.Label>
+          <Form.Control
+            placeholder="Enter password"
+            name="password"
+            type="password"
+            className="input-fields"
+            onChange={(e) => handleInput(e)}
+          />
+        </Form.Group>
+        <Form.Group className="mb-3">
+          <Form.Text className="text-muted">
+            <span>*</span> Field required.
+          </Form.Text>
+        </Form.Group>
+        <Form.Group className="d-grid d-md-flex justify-content-md-end">
+          <Button variant="info" type="submit" onClick={(e) => handleLogin(e)}>
+            Log in
+          </Button>
+        </Form.Group>{" "}
+        <Form.Group>
+          <Form.Label>Login in with</Form.Label>{" "}
+        </Form.Group>
+        <Form.Group>
+          <img
+            type="button"
+            className="svg"
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/2560px-Google_2015_logo.svg.png"
+            alt="google-logo"
+            onClick={(e) => handleLoginWithGoogle(e)}
+          />
+        </Form.Group>
+      </Form>
+    </div>
+  );
 }
