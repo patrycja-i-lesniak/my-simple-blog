185870ef6620bbdfb14103e820e9426fe57cf5df
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2022-08-24 20:49:50 +0200

    delete unused hook, add navigate to homepage, add event to logout function

--- src/components/layout/SignedInLinks.js
+++ src/components/layout/SignedInLinks.js
@@ -1,47 +1,35 @@
-import React, { useState, useEffect } from 'react';
-import { NavLink, useNavigate } from 'react-router-dom';
-import { useAuth, logout, colRef } from '../../config/firebaseConfig';
-import { getDocs, query, where } from 'firebase/firestore';
-import { Nav } from 'react-bootstrap';
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { Nav } from "react-bootstrap";
+import { signOut } from "firebase/auth";
+
+import { auth } from "../../config/firebase";
 
 export default function SignedInLinks() {
-	const [ loading, setLoading ] = useState(false);
-	const currentUser = useAuth();
-	const navigate = useNavigate();
-	const [ photoURL, setPhotoURL ] = useState(
-		'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png'
-	);
-	const [ title, setTitle ] = useState();
+  const [loading, setLoading] = useState(false);
+  const user = auth.currentUser;
+  const navigate = useNavigate();
 
-	useEffect(
-		() => {
-			{
-				currentUser && currentUser.photoURL
-					? setPhotoURL(currentUser.photoURL)
-					: setPhotoURL(photoURL);
-			}
-		},
-		[ currentUser ]
-	);
+  async function handleLogout(e) {
+    e.preventDefault();
+    setLoading(true);
+    signOut(auth)
+      .then(() => {
+        console.log("Log Out");
+      })
+      .catch((error) => {
+        alert("Error!");
+      });
+    setLoading(false);
+    navigate("/");
+  }
 
-	async function handleLogout() {
-		setLoading(true);
-		try {
-			await logout();
-			console.log('Log Out');
-		} catch (error) {
-			alert('Error!');
-		}
-		setLoading(false);
-		navigate('/');
-	}
-
-	return (
-		<Nav className="me-auto" hidden={loading || !currentUser}>
-			<Nav.Link href="/" onClick={handleLogout}>
-				Log Out
-			</Nav.Link>
-			<Nav.Link href="/profile">Your Profile</Nav.Link>
-		</Nav>
-	);
+  return (
+    <Nav className="me-auto" hidden={loading || !user}>
+      <Nav.Link href="/" onClick={(e) => handleLogout(e)}>
+        Log Out
+      </Nav.Link>
+      <Nav.Link href="/profile">Your Profile</Nav.Link>
+    </Nav>
+  );
 }
