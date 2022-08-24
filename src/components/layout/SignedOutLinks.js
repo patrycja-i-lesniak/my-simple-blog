5994d742d3a2c7c7d94147af5292850cdecbd2c3
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2022-08-24 20:51:08 +0200

    delete unnecessary import, correct bugs

--- src/components/layout/SignedOutLinks.js
+++ src/components/layout/SignedOutLinks.js
@@ -1,14 +1,15 @@
 import React from "react";
 import { Nav } from "react-bootstrap";
-import { auth, user } from "../../config/firebase";
+
+import { auth } from "../../config/firebase";
 
 export default function SignedOutLinks() {
   const user = auth.currentUser;
 
   return (
     <Nav className="me-auto" hidden={user}>
-      <Nav.Link href="/signup">Signup</Nav.Link>
-      <Nav.Link href="/login">Login</Nav.Link>
+      <Nav.Link href="/signup">Sign up</Nav.Link>
+      <Nav.Link href="/login">Log in</Nav.Link>
     </Nav>
   );
 }
