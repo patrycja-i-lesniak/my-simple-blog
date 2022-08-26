a455781cee808dadae3b06b88251536b06abb8aa
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2022-08-26 18:05:37 +0200

    simplify component

--- src/components/auth/Profile.js
+++ src/components/auth/Profile.js
@@ -1,51 +1,32 @@
-import React, { useState, useEffect } from 'react';
-import { useAuth, upload } from '../../config/firebaseConfig';
-import { Button, Form } from 'react-bootstrap';
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
+import { Button, Form } from "react-bootstrap";
+
+import { auth, useAuth } from "config/firebase";
+import UserDataForm from "./UserDataForm";
+import ProfileHeader from "./ProfileHeader";
 
 export default function Profile() {
-	const currentUser = useAuth();
-	const [ photo, setPhoto ] = useState(null);
-	const [ loading, setLoading ] = useState(false);
-	const [ photoURL, setPhotoURL ] = useState(
-		'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png'
-	);
+  const currentUser = useAuth();
+  const [loading, setLoading] = useState(false);
 
-	function handleChange(e) {
-		if (e.target.files[0]) {
-			setPhoto(e.target.files[0]);
-		}
-	}
+  const navigate = useNavigate();
 
-	function handleClick() {
-		upload(photo, currentUser, setLoading);
-	}
-	
+  function handleSubmit() {
+    navigate("/login");
+  }
 
-	useEffect(
-		() => {
-			if (currentUser && currentUser.photoURL) {
-				setPhotoURL(currentUser.photoURL);
-			}
-		},
-		[ currentUser ]
-	);
-
-console.log(photoURL);
-
-	return (
-		<div className="fields container">
-			{currentUser && (
-				<p>
-					Hello <span>{currentUser.email}</span>!
-				</p>
-				
-			)}
-
-			<input type="file" onChange={handleChange} />
-			<Button disabled={loading || !photo} onClick={handleClick} variant="btn btn-secondary">
-				Upload
-			</Button>
-			<img src={photoURL} alt="Avatar" className="avatar big" />
-		</div>
-	);
+  return (
+    <div className="fields-container">
+      <ProfileHeader />
+      {!currentUser && (
+        <div className="fields-container">
+          <Button variant="btn btn-info" onClick={handleSubmit}>
+            Go to login page
+          </Button>
+        </div>
+      )}
+    </div>
+  );
 }
