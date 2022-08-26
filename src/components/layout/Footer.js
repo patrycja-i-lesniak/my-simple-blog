db3c7183c9ab4618fc20078700f3c82f28fcdcc9
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2022-08-26 11:22:50 +0200

    rebuild footer with react-bootstrap

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,16 +1,15 @@
-import React from 'react';
-import {BsGithub} from 'react-icons/bs';
+import { Navbar, Nav } from "react-bootstrap";
+import { BsGithub } from "react-icons/bs";
 
 export default function Footer() {
   return (
-    <footer className="page-footer grey darken-3">
-    
-          <div className="footer-copyright">
-            <div className="container">
-            © 2022 Coded by Patrycja Leśniak
-            <a className="grey-text text-lighten-4 right" href="https://github.com/patrycja-i-lesniak"><BsGithub className='github'/></a>
-            </div>
-          </div>
-        </footer>
-  )
+    <Navbar fixed="bottom" bg="light" expand="lg" className="footer">
+      <Nav.Link href="https://github.com/patrycja-i-lesniak">
+        © 2022 Coded by Patrycja Leśniak
+      </Nav.Link>
+      <Nav.Link href="https://github.com/patrycja-i-lesniak">
+        <BsGithub className="github" />
+      </Nav.Link>
+    </Navbar>
+  );
 }
