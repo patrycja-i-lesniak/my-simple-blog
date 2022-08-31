611e24dc5c4c0af8f47fdd6aa532d7e32fb38230
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2022-08-31 21:46:26 +0200

    delete Dashboard, change paths

--- src/components/index.js
+++ src/components/index.js
@@ -1,13 +1,12 @@
 export {default as Login} from './auth/Login';
 export {default as SignUp} from './auth/SignUp';
-export {default as Profile} from './auth/Profile';
+export {default as Profile} from './auth/Profile/Profile';
 
 export {default as NavBar} from './layout/Navbar';
 export {default as Footer} from './layout/Footer';
 export {default as SearchBox} from './layout/SearchBox';
 export {default as Submenu} from './layout/Submenu';
 export {default as Slider} from './layout/Slider';
-export {default as Dashboard} from './layout/Dashboard';
 
 export {default as BlogDetails} from './blogs/BlogDetails';
 export {default as CreateBlog} from './blogs/CreateBlog';
