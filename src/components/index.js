8233b495a8962309c8c46c6f95578c5283de2089
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2022-09-04 21:26:56 +0200

    add new imports

--- src/components/index.js
+++ src/components/index.js
@@ -1,14 +1,18 @@
-export {default as Login} from './auth/Login';
-export {default as SignUp} from './auth/SignUp';
-export {default as Profile} from './auth/Profile/Profile';
+export { default as Login } from "./auth/Login";
+export { default as SignUp } from "./auth/SignUp";
+export { default as Profile } from "./auth/Profile/Profile";
+export { default as ProfileForm } from "./auth/Profile/ProfileForm";
 
-export {default as NavBar} from './layout/Navbar';
-export {default as Footer} from './layout/Footer';
-export {default as SearchBox} from './layout/SearchBox';
-export {default as Submenu} from './layout/Submenu';
-export {default as Slider} from './layout/Slider';
+export { default as NavBar } from "./layout/Navbar";
+export { default as Footer } from "./layout/Footer";
+export { default as SearchBox } from "./layout/SearchBox";
+export { default as Submenu } from "./layout/Submenu";
+export { default as Slider } from "./layout/Slider";
+export { default as ActionForm } from "./layout/ActionForm";
+export { default as GoogleLoginForm } from "./layout/GoogleLoginForm";
+export {default as AlertPopup} from './layout/AlertPopup';
 
-export {default as BlogDetails} from './blogs/BlogDetails';
-export {default as CreateBlog} from './blogs/CreateBlog';
-export {default as BlogList} from './blogs/BlogList';
-export {default as NewPage} from './blogs/NewPage';
+export { default as BlogDetails } from "./blogs/BlogDetails";
+export { default as CreateBlog } from "./blogs/CreateBlog";
+export { default as BlogList } from "./blogs/BlogList";
+export { default as NewPage } from "./blogs/NewPage";
