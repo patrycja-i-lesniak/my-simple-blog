ddd9c8cf0f5cbcab412886b1f777da541cbb0c88
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2022-10-20 21:02:29 +0200

    Updates

--- src/components/layout/Mailto.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export default function Mailto({ email, subject, body, ...props }) {
-  return (
-    <a href={`mailto:${email}?subject=${subject || ""}&body=${body || ""}`}>{props.children}</a>
-  );
-}
--- src/helpers/index.js
+++
@@ -1,4 +0,0 @@
-export { default as handleEdit } from './handleEdit';
-export { default as handleDelete } from './handleDelete';
-export { default as handleQuerySearch } from './handleQuerySearch';
-export { default as handleQueryDelete } from './handleQueryDelete';
