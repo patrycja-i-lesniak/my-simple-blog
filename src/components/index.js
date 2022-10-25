2fd261f4793f83b3c44b2a6e79dd7ef224f346c8
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2022-10-25 21:12:38 +0200

    delete deleted components exports

--- src/components/index.js
+++ src/components/index.js
@@ -1,20 +1,24 @@
-export { default as Login } from "./auth/Login";
+// auth
+export { default as SignIn } from "./auth/SignIn";
 export { default as SignUp } from "./auth/SignUp";
 export { default as Profile } from "./auth/Profile/Profile";
 export { default as ProfileForm } from "./auth/Profile/ProfileForm";
 
-export { default as NavBar } from "./layout/Navbar";
+// layout
 export { default as Footer } from "./layout/Footer";
 export { default as SearchBox } from "./layout/SearchBox";
-export { default as Submenu } from "./layout/Submenu";
+export { default as Submenu } from "./layout/Submenu/";
+export { default as MainMenu } from "./layout/MainMenu";
 export { default as Slider } from "./layout/Slider";
 export { default as ActionForm } from "./layout/ActionForm";
 export { default as GoogleLoginForm } from "./layout/GoogleLoginForm";
-export {default as AlertPopup} from './layout/AlertPopup';
-export {default as Mailto} from './layout/Mailto';
-export {default as Contact} from './layout/Contact';
+export { default as AlertPopup } from "./layout/AlertPopup";
+export { default as Mailto } from "./layout/Mailto";
+export { default as NotFoundPage } from "./layout/NotFoundPage";
 
+// blogs
 export { default as BlogDetails } from "./blogs/BlogDetails";
+export { default as BlogCard } from "./blogs/BlogCard";
 export { default as CreateBlog } from "./blogs/CreateBlog";
 export { default as BlogList } from "./blogs/BlogList";
 export { default as NewPage } from "./blogs/NewPage";
