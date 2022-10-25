cc9bf1b1993b220b1bf86b98bc013da888d85d92
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2022-10-25 21:08:47 +0200

    delete ThemePrivider

--- src/index.js
+++ src/index.js
@@ -1,24 +1,18 @@
-import React, { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import './index.css';
-import App from './App';
-import { ThemeProvider } from 'react-bootstrap';
+import React, { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
+import App from "./App";
 
-const rootElement = document.getElementById('root');
+const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
 root.render(
-	<StrictMode>
-		<BrowserRouter>
-			<ThemeProvider
-				breakpoints={[ 'xxxl', 'xxl', 'xl', 'lg', 'md', 'sm', 'xs', 'xxs' ]}
-				minBreakpoint="xxs"
-			>
-				<App />
-			</ThemeProvider>
-		</BrowserRouter>
-	</StrictMode>
+  <StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
