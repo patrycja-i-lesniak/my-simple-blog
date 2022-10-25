6f259ddf636a6a04bbd15631babee97f6f5b158f
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2022-10-25 21:15:50 +0200

    add Mailto component

--- src/components/layout/Mailto.js
+++ src/components/layout/Mailto.js
@@ -2,6 +2,6 @@ import React from 'react';
 
 export default function Mailto({ email, subject, body, ...props }) {
   return (
-    <a href={`mailto:${email}?subject=${subject || ""}&body=${body || ""}`}>{props.children}</a>
+    <a className='mailMe' href={`mailto:${email}?subject=${subject || ""}&body=${body || ""}`}>{props.children}</a>
   );
 }
