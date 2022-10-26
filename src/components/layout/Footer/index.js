a9434c7bbbfdf28f9086c6ba7de52cd49fe5af00
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2022-10-26 23:09:22 +0200

    move to separate folder

--- src/components/layout/Footer/index.js
+++ src/components/layout/Footer/index.js
@@ -1,17 +1,17 @@
 import { Navbar, Nav } from "react-bootstrap";
 import { BsGithub } from "react-icons/bs";
-import {Mailto} from 'components';
+import Mailto from './Mailto';
 
 export default function Footer() {
   return (
     <>
-      <Navbar fixed="bottom" bg="light" expand="lg" className="footer">
+      <Navbar fixed="bottom" bg="light" expand="lg" className="justify-content-center">
         <Nav.Link href="https://github.com/patrycja-i-lesniak">
           {" "}
           &copy; 2022 by Patrycja Leśniak
         </Nav.Link>
-        <Nav.Link href="https://github.com/patrycja-i-lesniak">
-          <BsGithub className="github" />
+        <Nav.Link href="https://github.com/patrycja-i-lesniak" className="github">
+          <BsGithub style={{margin:'0 10px'}}/>
         </Nav.Link>
         <Mailto email="patrycja.lesniak@gmail.com" subject="Hello" body="Hello world!">
           Mail me!
