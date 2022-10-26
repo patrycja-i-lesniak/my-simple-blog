f666dacdb9aef04c9e3e65a9894a81d0a1311ffb
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2022-10-26 22:34:45 +0200

    delete unnecessary elements and add ControlledCarousel

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,12 +1,10 @@
 import React from "react";
-import { BlogList } from "components";
-import { Slider } from "components";
+import { BlogList, ControlledCarousel } from "components";
 
 export default function Home() {
   return (
     <div>
-      <h1>HOME</h1>
-      <Slider />
+      <ControlledCarousel/>
       <BlogList />
     </div>
   );
