887de3063cc2570fb12b03bc1be27651a2f50047
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2022-11-02 21:11:33 +0100

    create Articles component and style elements

--- src/components/Articles/index.js
+++ src/components/Articles/index.js
@@ -1,11 +1,16 @@
 import React, { useState, useEffect } from "react";
 import { collection, onSnapshot, orderBy, query } from "firebase/firestore";
-import { db } from "../../firebaseConfig";
-
 import { Button, Card } from "react-bootstrap";
 
-export default function Article() {
+import "./style.css";
+import { db, useAuth } from "../../firebaseConfig";
+import { DeleteArticle } from "components";
+
+export default function Articles({ width }) {
   const [articles, setArticles] = useState([]);
+  const [isVisible, setVisible] = useState(false);
+
+  const currentUser = useAuth();
 
   useEffect(() => {
     const articleRef = collection(db, "Articles");
@@ -20,19 +25,34 @@ export default function Article() {
     });
   }, []);
 
+
   return (
-    <div>
+    <div className="card__container">
       {articles.length === 0 ? (
         <p>No article found!</p>
       ) : (
         articles.map(({ id, title, author, imageUrl, description, createdAt, articleBody }) => (
-          <Card key={id} style={{ width: "18rem" }}>
+          <Card className="card" key={id} style={{ width }}>
             <Card.Img variant="top" src={imageUrl} />
             <Card.Body>
               <Card.Title>{title}</Card.Title>
-              <Card.Text>{description}</Card.Text>
-              <Card.Text>{createdAt.toDate().toDateString()}</Card.Text>
-              <Button variant="info">Go somewhere</Button>
+              <Card.Text className="card__created">{createdAt.toDate().toDateString()}</Card.Text>
+              <Card.Text className="card__description">{description}</Card.Text>
+
+              {isVisible && (
+                <>
+                  <Card.Text>{articleBody}</Card.Text>
+                </>
+              )}
+
+              <div className="card__buttons">
+                
+                  <Button variant="info" onClick={() => setVisible(!isVisible)}>
+                    {isVisible ? 'show less' :' show more'}
+                  </Button>
+
+                {currentUser && <DeleteArticle id={id} imageUrl={imageUrl} />}
+              </div>
             </Card.Body>
           </Card>
         ))
