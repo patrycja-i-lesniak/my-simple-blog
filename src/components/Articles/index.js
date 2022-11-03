443eba2fbe1e1d30d770bfeeceff2430b72045d1
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2022-11-03 13:15:30 +0100

    add Article Description component, add author field to Card

--- src/components/Articles/index.js
+++ src/components/Articles/index.js
@@ -4,7 +4,8 @@ import { Button, Card } from "react-bootstrap";
 
 import "./style.css";
 import { db, useAuth } from "../../firebaseConfig";
-import { DeleteArticle } from "components";
+import { DeleteArticle, EditArticle, ArticleDescription } from "components";
+
 
 export default function Articles({ width }) {
   const [articles, setArticles] = useState([]);
@@ -25,7 +26,6 @@ export default function Articles({ width }) {
     });
   }, []);
 
-
   return (
     <div className="card__container">
       {articles.length === 0 ? (
@@ -37,21 +37,26 @@ export default function Articles({ width }) {
             <Card.Body>
               <Card.Title>{title}</Card.Title>
               <Card.Text className="card__created">{createdAt.toDate().toDateString()}</Card.Text>
-              <Card.Text className="card__description">{description}</Card.Text>
-
+              <ArticleDescription description={description} isVisible={isVisible}/>
               {isVisible && (
                 <>
                   <Card.Text>{articleBody}</Card.Text>
+                  <Card.Text className="card__author">
+                    Author: <span>{author}</span>
+                  </Card.Text>
                 </>
               )}
-
               <div className="card__buttons">
-                
-                  <Button variant="info" onClick={() => setVisible(!isVisible)}>
-                    {isVisible ? 'show less' :' show more'}
-                  </Button>
+                <Button variant="info" onClick={() => setVisible(!isVisible)}>
+                  {isVisible ? "Show less" : " Show more"}
+                </Button>
 
-                {currentUser && <DeleteArticle id={id} imageUrl={imageUrl} />}
+                {currentUser && (
+                  <div className="card__buttons--optional">
+                    <EditArticle id={id} imageUrl={imageUrl} />
+                    <DeleteArticle id={id} imageUrl={imageUrl} />
+                  </div>
+                )}
               </div>
             </Card.Body>
           </Card>
