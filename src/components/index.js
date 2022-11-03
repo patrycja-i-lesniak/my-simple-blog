c347cb6cb357535b1583ea90188c78b8ced5b42e
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2022-11-03 13:13:16 +0100

    export new components

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,6 @@
 // auth
 export { default as SignIn } from "./auth/SignIn";
-export { default as SignUp } from "./auth/SignUp";
+export { default as Register } from "./auth/Register";
 export { default as Profile } from "./auth/Profile/Profile";
 export { default as ProfileForm } from "./auth/Profile/ProfileForm";
 
@@ -13,9 +13,9 @@ export { default as ActionForm } from "./layout/ActionForm";
 export { default as GoogleLoginForm } from "./layout/GoogleLoginForm";
 export { default as AlertPopup } from "./layout/AlertPopup";
 export { default as NotFoundPage } from "./layout/NotFoundPage";
-export {default as AboutCard} from './layout/AboutCard';
+export { default as AboutCard } from "./layout/AboutCard";
 export { default as ControlledCarousel } from "./layout/ControlledCarousel";
-
+export { default as SmallModal } from "./layout/SmallModal";
 
 // blogs
 export { default as BlogDetails } from "./blogs/BlogDetails";
@@ -23,3 +23,10 @@ export { default as BlogCard } from "./blogs/BlogCard";
 export { default as CreateBlog } from "./blogs/CreateBlog";
 export { default as BlogList } from "./blogs/BlogList";
 
+// articles
+export { default as Articles } from "./Articles";
+export { default as AddArticle } from "./AddArticle";
+export { default as DeleteArticle } from "./DeleteArticle";
+export { default as ArticleDetails } from "./ArticleDetails";
+export { default as EditArticle } from "./EditArticle";
+export { default as ArticleDescription } from "./ArticleDescription";
