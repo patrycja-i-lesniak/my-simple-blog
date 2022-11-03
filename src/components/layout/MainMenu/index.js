482b393c42e5e55730585661338807d5541e1b84
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2022-11-03 15:55:10 +0100

    simplyfy MainMenu, separate the logic for the logged-in user into a separate component

--- src/components/layout/MainMenu/index.js
+++ src/components/layout/MainMenu/index.js
@@ -1,52 +1,29 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
-import { useAuth } from "config/firebaseConfig";
-import SignedInLiks from "./SignedInLinks";
+import SignedInLinks from "./SignedInLinks";
 import SignedOutLinks from "./SignedOutLinks";
-import { BiSearch } from "react-icons/bi";
-import { RiCloseLine } from "react-icons/ri";
-
-import blankProfilePicture from "assets/blank-profile-picture.png";
-
+import { useAuth } from "firebaseConfig";
 import "./style.css";
-
-import { SearchBox } from "components";
+import { SearchBox, SearchIcons } from "components";
 
 export default function MainMenu() {
-  const currentUser = useAuth();
-
-  // const [loading, setLoading] = useState(false);
-  const [photoURL, setPhotoURL] = useState(blankProfilePicture);
-
   const [isVisible, setIsVisible] = useState(true);
+  const currentUser = useAuth();
 
   function toggleIsVisible() {
     setIsVisible(!isVisible);
   }
 
-  useEffect(() => {
-    currentUser && currentUser.photoURL ? setPhotoURL(currentUser.photoURL) : setPhotoURL(photoURL);
-  }, [currentUser, photoURL]);
-
-
   return (
     <nav>
       <ul className="mainmenu">
-        <Link className="mainmenu__link mainmenu__link--header" to="/" alt="logo">
+        <Link className="mainmenu__header " to="/" alt="logo">
           Simple Vege Life
         </Link>
         <div className="mainmenu__container">
-          {isVisible ? (
-            <BiSearch className="search-icon" type="button" onClick={toggleIsVisible} />
-          ) : (
-            <RiCloseLine className="search-icon" onClick={toggleIsVisible} />
-          )}
-
-          <SignedInLiks />
-
-          <SignedOutLinks />
-          {currentUser && <img src={photoURL} alt="Avatar" className="avatar" />}
+          <SearchIcons isVisible={isVisible} toggleIsVisible={toggleIsVisible} />
+          {currentUser ? <SignedInLinks /> : <SignedOutLinks />}
         </div>
       </ul>
       {!isVisible && <SearchBox isVisible={isVisible} toggleIsVisible={toggleIsVisible} />}
