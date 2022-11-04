374197f1cde4ab2aca79a84064310f1f2c95eeb9
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2022-11-04 21:22:54 +0100

    add LikeArticle component

---
+++ src/components/LikeArticle/index.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth, db } from "firebaseConfig";
+import { doc, updateDoc, arrayRemove, arrayUnion } from "firebase/firestore";
+
+export default function LikeArticle({ id, likes }) {
+  const [user] = useAuthState(auth);
+  const likesRef = doc(db, "Articles", id);
+
+  const handleLike = () => {
+    if (likes?.includes(user.uid)) {
+      updateDoc(likesRef, {
+        likes: arrayRemove(user.uid),
+      })
+        .then(() => {
+          console.log("unliked");
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    } else {
+      updateDoc(likesRef, {
+        likes: arrayUnion(user.uid),
+      })
+        .then(() => {
+          console.log("liked");
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
+  };
+  return (
+    <div>
+      <i
+        className={`fa fa-heart${!likes?.includes(user.uid) ? "-o" : ""} fa-lg`}
+        style={{
+          cursor: "pointer",
+          color: likes?.includes(user.uid) ? "crimson" : null,
+        }}
+        onClick={handleLike}
+      />
+    </div>
+  );
+}
