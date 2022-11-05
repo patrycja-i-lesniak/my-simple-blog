311604307ab13607c568e8e8dd199d3a4f35eb60
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2022-11-05 20:01:11 +0100

    rebuild component, add LikeButton, style component

--- src/components/ArticleDetails/index.js
+++ src/components/ArticleDetails/index.js
@@ -3,14 +3,16 @@ import { useParams, useNavigate } from "react-router-dom";
 import { Card, Button } from "react-bootstrap";
 import { onSnapshot, doc } from "firebase/firestore";
 
-import { useAuth, db } from "firebaseConfig";
-import { DeleteArticle, EditArticle } from "components";
+import { db, auth } from "firebaseConfig";
+import { DeleteArticle, EditArticle, LikeButton} from "components";
 import "./style.css";
+import { useAuthState } from "react-firebase-hooks/auth";
 
 export default function ArticleDetails() {
   const [article, setArticle] = useState(null);
+  const [user] = useAuthState(auth);
   const { id } = useParams();
-  const currentUser = useAuth();
+
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -22,31 +24,38 @@ export default function ArticleDetails() {
   return (
     <div className="wrapper">
       {article && (
-        <Card className="card" key={id} style={{ width: "100%", maxWidth:1000 }}>
+        <Card className="card" key={id} style={{ width: "100%", maxWidth: 1000 }}>
           <Card.Img variant="top" src={article.imageUrl} />
-          <Card.Body className='article__body'>
-            <Card.Title className="article__title">{article.title}</Card.Title>
-            <Card.Text className='article__createdAt'>
-              Posted on: {article.createdAt.toDate().toDateString()}
-            </Card.Text>
-            <Card.Text className="article__description">{article.description}</Card.Text>
-            <hr/>
-            <Card.Text>{article.articleBody}</Card.Text>
-            <Card.Text>
-              Author: <span>{article.createdBy}</span>
-            </Card.Text>
-
-            <div className="article__icons">
-              <Button variant="outline-info" onClick={() => navigate('/articles')}>
-                Go back
-              </Button>
-              {/* <Button variant="info">read more</Button> */}
-              {currentUser && (
+          <Card.Body className="article__body">
+            <div className="article__edit">
+              {user && (
                 <div className="article__icons--user">
                   <EditArticle id={id} imageUrl={article.imageUrl} />
                   <DeleteArticle id={id} imageUrl={article.imageUrl} />
                 </div>
               )}
+            </div>
+            <Card.Title className="article__title">{article.title}</Card.Title>
+
+            <Card.Text className="article__createdAt">
+              Posted on: {article.createdAt.toDate().toDateString()}
+            </Card.Text>
+
+            <Card.Text className="article__description">{article.description}</Card.Text>
+            <hr />
+            <Card.Text>{article.articleBody}</Card.Text>
+            <Card.Text className="article__author">
+              Author: <span>{article.createdBy}</span>
+            </Card.Text>
+
+            <div className="article__icons">
+              <Button variant="outline-info" onClick={() => navigate("/articles")}>
+                Go back
+              </Button>
+              <div className="article__likes">
+                {user && <LikeButton id={id} likes={article.likes} />}
+                <p>{article.likes.length}</p>
+              </div>
             </div>
           </Card.Body>
         </Card>
