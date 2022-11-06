592d4541cefe721809d57aca0f8a7369abe9c095
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2022-11-06 19:16:22 +0100

    add to handleDelete function navigate to artilces

--- src/components/DeleteArticle/index.js
+++ src/components/DeleteArticle/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import { deleteDoc, doc } from "firebase/firestore";
 import { deleteObject, ref } from "firebase/storage";
 import { toast } from "react-toastify";
@@ -6,7 +7,9 @@ import { FiTrash } from "react-icons/fi";
 
 import { db, storage } from "../../firebaseConfig";
 
-export default function index({ id, imageUrl }) {
+export default function DeleteArticle({ id, imageUrl }) {
+  const navigate = useNavigate();
+
   const handleDelete = async () => {
     if (window.confirm("Are you sure you want to delete this article?")) {
       try {
@@ -14,6 +17,7 @@ export default function index({ id, imageUrl }) {
         toast("Article deleted successfully", { type: "succes" });
         const storageRef = ref(storage, imageUrl);
         await deleteObject(storageRef);
+        navigate("/articles");
       } catch (error) {
         toast("Error deleting article", { type: "error" });
         console.log(error);
