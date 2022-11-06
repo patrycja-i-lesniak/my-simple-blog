529bd7c6a3e88cbf2b5bb9ed40712568789c14ff
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2022-11-06 19:01:07 +0100

    clean code

--- src/App.js
+++ src/App.js
@@ -1,21 +1,18 @@
 import React from "react";
-import { Route, Routes} from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 // components
 import { NotFoundPage, Register, Footer, Submenu, MainMenu } from "components";
 import { Home, About, Contact } from "pages";
 import { ArticlesRoutes, LoginRoutes } from "routes";
 
-
 function App() {
-
   return (
     <>
       <header>
         <MainMenu />
+        <Submenu />
       </header>
-
-      <Submenu />
       <main>
         <Routes>
           <Route path="/" element={<Home />} />
