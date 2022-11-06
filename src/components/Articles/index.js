f3ec262d4e3ba8195dfea1413bd8a1c1a05d9fc7
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2022-11-06 22:18:10 +0100

    delete unnecessary props, restyle Card elements

--- src/components/Articles/index.js
+++ src/components/Articles/index.js
@@ -8,7 +8,7 @@ import "./style.css";
 import { db, auth } from "firebaseConfig";
 import { LikeButton } from "components";
 
-export default function Articles({ width }) {
+export default function Articles() {
   const [user] = useAuthState(auth);
   const [articles, setArticles] = useState([]);
 
@@ -23,12 +23,11 @@ export default function Articles({ width }) {
         ...doc.data(),
       }));
       setArticles(articles);
-      console.log(articles);
     });
   }, []);
 
   return (
-    <div className="row row-cols-1 row-cols-md-2 justify-content-between mb-60">
+    <div className=" card-columns justify-content-left ">
       {articles.length === 0 ? (
         <p>No article found!</p>
       ) : (
@@ -45,39 +44,43 @@ export default function Articles({ width }) {
             likes,
             comments,
           }) => (
-            <Card className="card px-0" key={id} style={{ width }}>
-              <Card.Img variant="top" src={imageUrl} />
-              <Card.Body>
-                <Card.Title>{title}</Card.Title>
-                <Card.Text className="card__created">{createdAt.toDate().toDateString()}</Card.Text>
-                <Card.Text className="card__description">{description}</Card.Text>
-                <div className="card__buttons">
-                  <Button
-                    variant="info"
-                    onClick={() => navigate(`/articles/${id}`, { replace: true })}
-                  >
-                    read more
-                  </Button>
-                </div>
-                <div className="card__iconsContainer">
-                  <div className="d-flex ">
-                    {user ? (
-                      <LikeButton id={id} likes={likes} />
-                    ) : (
-                      <i
-                        className="fa fa-heart  fa-lg"
-                        cursor="pointer"
-                        onClick={() => alert("Log in to add like")}
-                      />
-                    )}
-                    <div className="pe-2 px-3">
-                      <p>{likes?.length} likes</p>
-                    </div>
-                    {comments?.length > 0 && <p className="px-3">{comments?.length} comments</p>}
+            <div className="col-sm-12" key={id}>
+              <Card className="card px-0">
+                <Card.Img variant="top" src={imageUrl} />
+                <Card.Body>
+                  <Card.Title>{title}</Card.Title>
+                  <Card.Text className="card__created">
+                    {createdAt.toDate().toDateString()}
+                  </Card.Text>
+                  <Card.Text className="card__description">{description}</Card.Text>
+                  <div className="card__buttons">
+                    <Button
+                      variant="info"
+                      onClick={() => navigate(`/articles/${id}`, { replace: true })}
+                    >
+                      read more
+                    </Button>
                   </div>
-                </div>
-              </Card.Body>
-            </Card>
+                  <div className="card__iconsContainer">
+                    <div className="d-flex ">
+                      {user ? (
+                        <LikeButton id={id} likes={likes} />
+                      ) : (
+                        <i
+                          className="fa fa-heart  fa-lg"
+                          cursor="pointer"
+                          onClick={() => alert("Log in to add like")}
+                        />
+                      )}
+                      <div className="pe-2 px-3">
+                        <p>{likes?.length} likes</p>
+                      </div>
+                      {comments?.length > 0 && <p className="px-3">{comments?.length} comments</p>}
+                    </div>
+                  </div>
+                </Card.Body>
+              </Card>
+            </div>
           )
         )
       )}
