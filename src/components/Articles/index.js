173730ee214a5fa242282e24c717b380ec6c30a6
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2022-11-06 19:05:59 +0100

    eplace navigate to home page with article page

--- src/components/Articles/index.js
+++ src/components/Articles/index.js
@@ -1,17 +1,19 @@
 import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import { collection, onSnapshot, orderBy, query } from "firebase/firestore";
 import { Button, Card } from "react-bootstrap";
+import { useAuthState } from "react-firebase-hooks/auth";
 
 import "./style.css";
-import { db, useAuth } from "../../firebaseConfig";
-import { DeleteArticle, EditArticle, ArticleDescription } from "components";
-
+import { db, auth } from "firebaseConfig";
+import { DeleteArticle, EditArticle, ArticleDescription, LikeButton } from "components";
 
 export default function Articles({ width }) {
+  const [user] = useAuthState(auth);
   const [articles, setArticles] = useState([]);
   const [isVisible, setVisible] = useState(false);
 
-  const currentUser = useAuth();
+  const navigate = useNavigate();
 
   useEffect(() => {
     const articleRef = collection(db, "Articles");
@@ -31,36 +33,79 @@ export default function Articles({ width }) {
       {articles.length === 0 ? (
         <p>No article found!</p>
       ) : (
-        articles.map(({ id, title, author, imageUrl, description, createdAt, articleBody }) => (
-          <Card className="card" key={id} style={{ width }}>
-            <Card.Img variant="top" src={imageUrl} />
-            <Card.Body>
-              <Card.Title>{title}</Card.Title>
-              <Card.Text className="card__created">{createdAt.toDate().toDateString()}</Card.Text>
-              <ArticleDescription description={description} isVisible={isVisible}/>
-              {isVisible && (
-                <>
-                  <Card.Text>{articleBody}</Card.Text>
-                  <Card.Text className="card__author">
-                    Author: <span>{author}</span>
-                  </Card.Text>
-                </>
-              )}
-              <div className="card__buttons">
-                <Button variant="info" onClick={() => setVisible(!isVisible)}>
-                  {isVisible ? "Show less" : " Show more"}
-                </Button>
-
-                {currentUser && (
-                  <div className="card__buttons--optional">
-                    <EditArticle id={id} imageUrl={imageUrl} />
-                    <DeleteArticle id={id} imageUrl={imageUrl} />
-                  </div>
+        articles.map(
+          ({
+            id,
+            title,
+            imageUrl,
+            description,
+            createdAt,
+            articleBody,
+            createdBy,
+            userId,
+            likes,
+            comments,
+          }) => (
+            <Card className="card" key={id} style={{ width }}>
+              <Card.Img variant="top" src={imageUrl} />
+              <Card.Body>
+                <Card.Title>{title}</Card.Title>
+                <Card.Text className="card__created">{createdAt.toDate().toDateString()}</Card.Text>
+                <ArticleDescription description={description} isVisible={isVisible} />
+                {isVisible && (
+                  <>
+                    <Card.Text>{articleBody}</Card.Text>
+                    {createdBy && <Card.Text className="card__createdBy">{createdBy}</Card.Text>}
+                  </>
                 )}
-              </div>
-            </Card.Body>
-          </Card>
-        ))
+
+                <div className="card__buttons">
+                  <Button
+                    variant="info"
+                    // as={Link}
+                    // to={`articles/${id}`}
+                    // onClick={() => setVisible(!isVisible)}
+                    onClick={() => navigate(`/articles/${id}`, { replace: true })}
+                  >
+                    {/* {isVisible ? "Show less" : " Show more"} */}
+                    read more
+                  </Button>
+
+                  {/* {user && (
+                    <div className="card__buttons--optional">
+                      <EditArticle id={id} imageUrl={imageUrl} />
+                      <DeleteArticle id={id} imageUrl={imageUrl} />
+                    </div>
+                  )} */}
+                </div>
+                <div className="card__iconsContainer">
+                  <div className="d-flex ">
+                    {user ? (
+                      <LikeButton id={id} likes={likes} />
+                    ) : (
+                      <i
+                        className="fa fa-heart  fa-lg"
+                        cursor="pointer"
+                        onClick={() => alert("Log in to add like")}
+                      />
+                    )}
+                    <div className="pe-2 px-3">
+                      <p>{likes?.length} likes</p>
+                    </div>
+                    {comments?.length > 0 && <p className="px-3">{comments?.length} comments</p>}
+                  </div>
+
+                  {user?.uid === userId && (
+                    <div className="card__icons--optional">
+                      <EditArticle id={id} imageUrl={imageUrl} />
+                      <DeleteArticle id={id} imageUrl={imageUrl} />
+                    </div>
+                  )}
+                </div>
+              </Card.Body>
+            </Card>
+          )
+        )
       )}
     </div>
   );
