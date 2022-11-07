68abeb1f565afda0aa70e298602e75db4d24e87c
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2022-11-07 23:29:15 +0100

    change logic

--- src/components/DeleteArticle/index.js
+++ src/components/DeleteArticle/index.js
@@ -13,11 +13,11 @@ export default function DeleteArticle({ id, imageUrl }) {
   const handleDelete = async () => {
     if (window.confirm("Are you sure you want to delete this article?")) {
       try {
+        navigate("/");
         await deleteDoc(doc(db, "Articles", id));
         toast("Article deleted successfully", { type: "succes" });
         const storageRef = ref(storage, imageUrl);
         await deleteObject(storageRef);
-        navigate("/articles");
       } catch (error) {
         toast("Error deleting article", { type: "error" });
         console.log(error);
