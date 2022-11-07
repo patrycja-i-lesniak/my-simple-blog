90150a4e556147376970fcc54b5d39922276e929
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2022-11-07 23:15:43 +0100

    add navigate to login page

--- src/components/auth/Profile/Profile.js
+++ src/components/auth/Profile/Profile.js
@@ -2,8 +2,10 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { Button, Spinner } from "react-bootstrap";
 
-import { useAuth } from "config/firebaseConfig";
+// import { useAuth } from "config/firebaseConfig";
 import ProfileHeader from "./ProfileHeader";
+
+import { useAuth } from "config/firebase";
 
 export default function Profile() {
   const currentUser = useAuth();
@@ -12,17 +14,17 @@ export default function Profile() {
   const navigate = useNavigate();
 
   function handleSubmit() {
-    navigate("/signin");
+    navigate("/login");
   }
 
   return (
     <div className="fields-container">
       {loading && (
-        <>
+        <div>
           <Spinner animation="grow" variant="info">
             <span className="visually-hidden">Loading...</span>
           </Spinner>
-        </>
+        </div>
       )}
       <ProfileHeader />
       {!currentUser && (
