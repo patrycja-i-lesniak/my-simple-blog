9946cfe4adac623c3067cee15dd694a2f4856678
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2022-11-07 23:31:37 +0100

    export pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
-export {default as Home} from './Home';
-export {default as Contact} from './Contact';
-export {default as AllArticles} from './AllArticles';
-export {default as ArticlePage} from './ArticlePage';
-export {default as AddNewArticle} from './AddNewArticle';
-export {default as About} from './About';
+export { default as Home } from "./Home";
+export { default as Contact } from "./Contact";
+export { default as AllArticles } from "./AllArticles";
+export { default as ArticlePage } from "./ArticlePage";
+export { default as AddNewArticle } from "./AddNewArticle";
+export { default as About } from "./About";
+export { default as ThanksPage } from "./ThanksPage";
