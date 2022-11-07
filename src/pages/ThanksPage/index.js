21cedfbbfb92cb005654ff3e3454a83d1cf2af9c
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2022-11-07 23:30:37 +0100

    add Thanks component

---
+++ src/pages/ThanksPage/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Thanks } from "components";
+
+export default function ThanksPage() {
+  return (
+    <div className="container">
+      <div className="row justify-content-center">
+        <div className="col-md-8">
+          <Thanks />
+        </div>
+      </div>
+    </div>
+  );
+}
