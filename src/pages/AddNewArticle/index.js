d3a7f4245f79eae0126a0d28abb9ac60f55d6aa9
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2022-11-08 19:41:30 +0100

    change container size

--- src/pages/AddNewArticle/index.js
+++ src/pages/AddNewArticle/index.js
@@ -5,7 +5,7 @@ export default function AddNewArticle() {
   return (
     <div className="p-3 pb-5">
       <div className="row justify-content-center m-3">
-        <div className="col-sm-10 col-md-8 bg-light rounded-lg px-3">
+        <div className="col-sm-10 col-md-9 col-lg-8  col-xl-6 bg-light rounded-lg px-3">
           <AddArticle />
         </div>
       </div>
--- src/pages/AllArticles/index.js
+++ src/pages/AllArticles/index.js
@@ -6,7 +6,7 @@ export default function AllArticles() {
   return (
     <div className="p-3 pb-5">
       <div className="row justify-content-center">
-        <div className="col-sm-12 col-lg-10 col-xl-8">
+        <div className="col-sm-11 col-lg-10 col-xl-8">
           <Articles width="100%" />
         </div>
       </div>
