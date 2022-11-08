7f855f2f804ede37e06312d147d54213329feb73
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2022-11-08 19:29:26 +0100

    add padding and clean code

--- src/pages/AllArticles/index.js
+++ src/pages/AllArticles/index.js
@@ -4,7 +4,7 @@ import { Articles } from "components";
 
 export default function AllArticles() {
   return (
-    <div className="pb-5">
+    <div className="p-3 pb-5">
       <div className="row justify-content-center">
         <div className="col-sm-12 col-lg-10 col-xl-8">
           <Articles width="100%" />
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,15 +2,13 @@ import React from "react";
 import { Articles, ControlledCarousel } from "components";
 
 export default function Home() {
-
-
   return (
     <div className="pb-5">
       <ControlledCarousel />
-      <div className="row justify-content-center ">
-        <div className="col-sm-12 col-lg-10 col-xl-8" >
+      <div className="row justify-content-center">
+        <div className="col-sm-12 col-lg-10 col-xl-8">
           <h1>Recent articles</h1>
-          <Articles size='5'/>
+          <Articles size="5" />
         </div>
       </div>
     </div>
